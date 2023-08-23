36081afbdc5e1d7764aba6f0f4db08b6bf8ac73a
hilleld <hillel.darshan@mail.huji.ac.il>
2023-08-23 16:10:11 +0300

    TBC. some fun with the last df :)

--- PMI_density.py
+++ PMI_density.py
@@ -32,6 +32,8 @@ def calculate_patients_df():
     lab_id = pd.read_csv(working_dir + "lab_name_id_table.csv")
     lab_id = lab_id.merge(pmi_projid, on='projid', how='left')
     lab_id = lab_id.merge(cold_projid, on='projid', how='left')
+
+    # keep only the cols we want
     cols_to_drop = list(lab_id.columns)
     cols_to_drop = [e for e in cols_to_drop if e not in ('Name', 'projid', 'pmi', 'cogdx', 'msex',
                                                          'ad_reagan', 'braaksc')]
@@ -58,4 +60,41 @@ if __name__ == '__main__':
     patients_df = calculate_patients_df()
     sums_df = calculate_sums_df(df)
 
+    sums_df_t = sums_df.T
+    new_columns = [f"{sums_df_t.iloc[0, i]}_{sums_df_t.iloc[1, i]}" for i in range(len(sums_df_t.columns))]
+    sums_df_t.columns = new_columns
+    sums_df_t = sums_df_t.iloc[2:]
+    sums_df_t['Name'] = sums_df_t.index
+    sums_df_t['Name'] = sums_df_t['Name'].str.split('_').str[:2].str.join(' ')
+    sums_df_t = sums_df_t.merge(patients_df, on='Name', how='left', sort=False)
+
+    intercept_lst = [
+                     ('AD pmi <=5', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 5)),
+                     ('AD 5<pmi<=7', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 7)  & (sums_df_t['pmi'] > 5)),
+                     ('AD 7<pmi<=9', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 9)  & (sums_df_t['pmi'] > 7)),
+                     ('AD 9<pmi<=11', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 11)  & (sums_df_t['pmi'] > 9)),
+                     ('AD 11<pmi<=13', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 13)  & (sums_df_t['pmi'] > 11)),
+                     ('AD 13<pmi<=15', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 15)  & (sums_df_t['pmi'] > 13)),
+                     ('AD 15<pmi', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] > 15)),
+
+                     ('MMCI pmi <=5', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 5)),
+                     ('MMCI 5<pmi<=7', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 7) & (sums_df_t['pmi'] > 5)),
+                     ('MMCI 7<pmi<=9', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 9) & (sums_df_t['pmi'] > 7)),
+                     ('MMCI 9<pmi<=11', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 11) & (sums_df_t['pmi'] > 9)),
+                     ('MMCI 11<pmi<=13', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 13) & (sums_df_t['pmi'] > 11)),
+                     ('MMCI 13<pmi<=15', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 15) & (sums_df_t['pmi'] > 13)),
+                     ('MMCI 15<pmi', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] > 15)),
+
+                     ('NCI pmi <=5', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 5)),
+                     ('NCI 5<pmi<=7', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 7) & (sums_df_t['pmi'] > 5)),
+                     ('NCI 7<pmi<=9', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 9) & (sums_df_t['pmi'] > 7)),
+                     ('NCI 9<pmi<=11', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 11) & (sums_df_t['pmi'] > 9)),
+                     ('NCI 11<pmi<=13', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 13) & (sums_df_t['pmi'] > 11)),
+                     ('NCI 13<pmi<=15', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 15) & (sums_df_t['pmi'] > 13)),
+                     ('NCI 15<pmi', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] > 15))
+                     ]
+    for intercept in intercept_lst:
+        new_row = sums_df_t[intercept[1]].sum()
+        new_row.name = intercept[0]
+        sums_df_t = sums_df_t.append(new_row)
     print("All Done")

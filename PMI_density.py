c5d5cc3e15dd629106eed83a5a14a6a06ca4f18c
hilleld <hillel.darshan@mail.huji.ac.il>
2023-08-24 15:40:33 +0300

    some density plot shows

--- PMI_density.py
+++ PMI_density.py
@@ -3,7 +3,8 @@
 from itertools import product
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def calculate_sums_df(patient_df):
@@ -13,16 +14,16 @@ def calculate_sums_df(patient_df):
     condition_combinations = list(product(tRF_groups, lens))
     patient_columns = list(patient_df.columns)[2:-2]
     # calculate sums
-    cur_sums_df = pd.DataFrame(columns=['tRF type(s)', 'len'] + patient_columns)
+    sums_df = pd.DataFrame(columns=['tRF type(s)', 'len'] + patient_columns)
     for condition_combination in condition_combinations:
         trf_type, trf_len = condition_combination
         sum_condition = (patient_df['tRF type(s)'] == trf_type) & (patient_df['len'] == trf_len)
 
         if sum_condition.any():  # Check if there are matching rows
             sum_result = patient_df[sum_condition][patient_columns].sum()
-            cur_sums_df.loc[len(cur_sums_df)] = [trf_type, trf_len] + list(sum_result)
+            sums_df.loc[len(sums_df)] = [trf_type, trf_len] + list(sum_result)
 
-    return cur_sums_df
+    return sums_df
 
 
 def calculate_patients_df():
@@ -54,47 +55,100 @@ def calculate_trf_df():
     return trf_df
 
 
-if __name__ == '__main__':
-    working_dir = "../PMI_analysis/working_data_PMI/PMI_Adi_DATA/"
-    df = calculate_trf_df()
-    patients_df = calculate_patients_df()
-    sums_df = calculate_sums_df(df)
-
+def calculate_result_df(sums_df):
+    # transpose matrix, and re-name cols to be names of patients
     sums_df_t = sums_df.T
     new_columns = [f"{sums_df_t.iloc[0, i]}_{sums_df_t.iloc[1, i]}" for i in range(len(sums_df_t.columns))]
     sums_df_t.columns = new_columns
     sums_df_t = sums_df_t.iloc[2:]
-    sums_df_t['Name'] = sums_df_t.index
-    sums_df_t['Name'] = sums_df_t['Name'].str.split('_').str[:2].str.join(' ')
+    sums_df_t['Name'] = sums_df_t.index.str.split('_').str[:2].str.join(' ')
     sums_df_t = sums_df_t.merge(patients_df, on='Name', how='left', sort=False)
 
     intercept_lst = [
-                     ('AD pmi <=5', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 5)),
-                     ('AD 5<pmi<=7', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 7)  & (sums_df_t['pmi'] > 5)),
-                     ('AD 7<pmi<=9', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 9)  & (sums_df_t['pmi'] > 7)),
-                     ('AD 9<pmi<=11', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 11)  & (sums_df_t['pmi'] > 9)),
-                     ('AD 11<pmi<=13', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 13)  & (sums_df_t['pmi'] > 11)),
-                     ('AD 13<pmi<=15', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 15)  & (sums_df_t['pmi'] > 13)),
-                     ('AD 15<pmi', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] > 15)),
+        ('AD pmi<=5', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 5)),
+        ('AD 5<pmi<=7', ((sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 7) & (sums_df_t['pmi'] > 5))),
+        ('AD 7<pmi<=9', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 9) & (sums_df_t['pmi'] > 7)),
+        ('AD 9<pmi<=11', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 11) & (sums_df_t['pmi'] > 9)),
+        ('AD 11<pmi<=13', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 13) & (sums_df_t['pmi'] > 11)),
+        ('AD 13<pmi<=15', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] <= 15) & (sums_df_t['pmi'] > 13)),
+        ('AD 15<pmi', (sums_df_t['cogdx'] == 4) & (sums_df_t['pmi'] > 15)),
 
-                     ('MMCI pmi <=5', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 5)),
-                     ('MMCI 5<pmi<=7', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 7) & (sums_df_t['pmi'] > 5)),
-                     ('MMCI 7<pmi<=9', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 9) & (sums_df_t['pmi'] > 7)),
-                     ('MMCI 9<pmi<=11', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 11) & (sums_df_t['pmi'] > 9)),
-                     ('MMCI 11<pmi<=13', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 13) & (sums_df_t['pmi'] > 11)),
-                     ('MMCI 13<pmi<=15', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 15) & (sums_df_t['pmi'] > 13)),
-                     ('MMCI 15<pmi', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] > 15)),
+        ('MMCI pmi<=5', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 5)),
+        ('MMCI 5<pmi<=7', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 7) & (sums_df_t['pmi'] > 5)),
+        ('MMCI 7<pmi<=9', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 9) & (sums_df_t['pmi'] > 7)),
+        ('MMCI 9<pmi<=11', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 11) & (sums_df_t['pmi'] > 9)),
+        ('MMCI 11<pmi<=13', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 13) & (sums_df_t['pmi'] > 11)),
+        ('MMCI 13<pmi<=15', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] <= 15) & (sums_df_t['pmi'] > 13)),
+        ('MMCI 15<pmi', (sums_df_t['cogdx'] == 2) & (sums_df_t['pmi'] > 15)),
 
-                     ('NCI pmi <=5', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 5)),
-                     ('NCI 5<pmi<=7', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 7) & (sums_df_t['pmi'] > 5)),
-                     ('NCI 7<pmi<=9', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 9) & (sums_df_t['pmi'] > 7)),
-                     ('NCI 9<pmi<=11', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 11) & (sums_df_t['pmi'] > 9)),
-                     ('NCI 11<pmi<=13', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 13) & (sums_df_t['pmi'] > 11)),
-                     ('NCI 13<pmi<=15', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 15) & (sums_df_t['pmi'] > 13)),
-                     ('NCI 15<pmi', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] > 15))
-                     ]
+        ('NCI pmi<=5', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 5)),
+        ('NCI 5<pmi<=7', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 7) & (sums_df_t['pmi'] > 5)),
+        ('NCI 7<pmi<=9', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 9) & (sums_df_t['pmi'] > 7)),
+        ('NCI 9<pmi<=11', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 11) & (sums_df_t['pmi'] > 9)),
+        ('NCI 11<pmi<=13', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 13) & (sums_df_t['pmi'] > 11)),
+        ('NCI 13<pmi<=15', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] <= 15) & (sums_df_t['pmi'] > 13)),
+        ('NCI 15<pmi', (sums_df_t['cogdx'] == 1) & (sums_df_t['pmi'] > 15))
+    ]
+    res_dfs = []
     for intercept in intercept_lst:
         new_row = sums_df_t[intercept[1]].sum()
         new_row.name = intercept[0]
-        sums_df_t = sums_df_t.append(new_row)
+        res_dfs.append(new_row)
+    result_df = pd.concat(res_dfs, axis=1)
+
+    result_df = result_df.iloc[:-7] # delete last rows, sums of meta-data
+
+    return result_df
+
+
+if __name__ == '__main__':
+    working_dir = "../PMI_analysis/working_data_PMI/PMI_Adi_DATA/"
+    df = calculate_trf_df()
+    patients_df = calculate_patients_df()
+    cur_sums_df = calculate_sums_df(df)
+    cur_res_df = calculate_result_df(cur_sums_df)
+
+    res_df = cur_res_df
+    res_df['type'] = res_df.index.str.split('_').str[:1].str.join(' ')
+    res_df['len'] = res_df.index.str.split('_').str[1]
+
+    trf_3 = res_df[res_df['type'] == '3\'-tRF']
+    trf_5 = res_df[res_df['type'] == '5\'-tRF']
+    trf_i = res_df[res_df['type'] == 'i-tRF']
+    trf_3_half = res_df[res_df['type'] == '3\'-half']
+    trf_5_half = res_df[res_df['type'] == '5\'-half']
+
+    # # Create subplots for multiple density plots
+    # fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(15, 5), sharey=True)
+    #
+    # # Plot density plots for each condition
+    # trf_3[['AD pmi<=5', 'len']].plot(kind='density', ax=axes[0], title='3\'-tRF')
+    # trf_5[['AD pmi<=5', 'len']].plot(kind='density', ax=axes[1], title='5\'-tRF')
+    # trf_i[['AD pmi<=5', 'len']].plot(kind='density', ax=axes[2], title='i-tRF')
+    # trf_3_half[['AD pmi<=5', 'len']].plot(kind='density', ax=axes[3], title='3\'-half')
+    # trf_5_half[['AD pmi<=5', 'len']].plot(kind='density', ax=axes[4], title='5\'-half')
+    #
+    # # Adjust layout and show the plots
+    # plt.tight_layout()
+    # plt.show()
+
+    # Create subplots for multiple density plots
+    fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(15, 5), sharey=True)
+
+    # Plot density plots for each condition in each subplot
+    trf_3[['AD pmi<=5', 'AD 5<pmi<=7', 'AD 7<pmi<=9', 'AD 9<pmi<=11', 'len']].plot(kind='density', ax=axes[0],
+                                                                                   title='3\'-tRF')
+    trf_5[['AD pmi<=5', 'AD 5<pmi<=7', 'AD 7<pmi<=9', 'AD 9<pmi<=11', 'len']].plot(kind='density', ax=axes[1],
+                                                                                   title='5\'-tRF')
+    trf_i[['AD pmi<=5', 'AD 5<pmi<=7', 'AD 7<pmi<=9', 'AD 9<pmi<=11', 'len']].plot(kind='density', ax=axes[2],
+                                                                                   title='i-tRF')
+    trf_3_half[['AD pmi<=5', 'AD 5<pmi<=7', 'AD 7<pmi<=9', 'AD 9<pmi<=11', 'len']].plot(kind='density', ax=axes[3],
+                                                                                        title='3\'-half')
+    trf_5_half[['AD pmi<=5', 'AD 5<pmi<=7', 'AD 7<pmi<=9', 'AD 9<pmi<=11', 'len']].plot(kind='density', ax=axes[4],
+                                                                                        title='5\'-half')
+
+    # Adjust layout and show the plots
+    plt.tight_layout()
+    plt.show()
+
     print("All Done")
